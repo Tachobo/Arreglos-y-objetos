53d89ae5d5149a7cb1f219c504a2f89a78bcd69f
Tachobo <wilmerardila2001@gmail.com>
2025-11-10 21:43:50 -0500

    Primer commit: Manipulación de objetos

---
+++ b.js
@@ -0,0 +1,5 @@
+let verduras = ["Zanahoria", "Lechuga", "Espinaca", "Cebolla"];
+
+for(let i = 0; i < verduras.length; i++){
+    console.log("Valor:", verduras[i], "  Posición:", i);
+}
---
+++ verduras.js
@@ -0,0 +1,16 @@
+let verduras = ["Zanahoria", "Lechuga", "Espinaca", "Cebolla"];
+// console.log(verduras);
+
+// verduras.push("Col");
+// console.log(verduras);
+
+// verduras.pop();
+// console.log(verduras);
+
+// verduras.unshift("Brocoli");
+// console.log(verduras);
+
+// verduras.shift();
+// console.log(verduras);
+
+console.log(verduras.length);
