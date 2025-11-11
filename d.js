96c4bc50f606bd2ef0339cb24ecc15a897e26f7d
Tachobo <wilmerardila2001@gmail.com>
2025-11-10 22:35:17 -0500

    Ultimo Commit: Arreglo de Objetos (Integración)

---
+++ d.js
@@ -0,0 +1,9 @@
+let inventario = [
+{nombre: "Zapato", talla: 39, color: "Negro"},
+{nombre: "Buzo", talla: "M", color: "Gris"},
+{nombre: "Pantalon", talla: 42, color: "Azul"},
+];
+
+for (let ropa of inventario){
+    console.log("Ropa:", ropa.talla);
+}
