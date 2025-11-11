3593f833050ca9edd8edaa7692c59ab2e70aac1d
Tachobo <wilmerardila2001@gmail.com>
2025-11-10 22:24:53 -0500

    Segundo commit: construcción y lectura de objetos

--- b.js
+++ b.js
@@ -1,5 +1,10 @@
 let verduras = ["Zanahoria", "Lechuga", "Espinaca", "Cebolla"];
 
-for(let i = 0; i < verduras.length; i++){
-    console.log("Valor:", verduras[i], "  Posición:", i);
-}
+// for(let i = 0; i < verduras.length; i++){
+//     console.log("Valor:", verduras[i], "  Posición:", i);
+// }
+
+// for(let verdura of verduras){
+//     console.log("Verdura:",  verdura);
+// }
+
---
+++ c.js
@@ -0,0 +1,10 @@
+let computador = {
+    Marca: "HP",
+    Procesador: "I5",
+    Ram: "8 GB",
+    Almacenamiento: "237 GB",
+};
+console.log("Marca: ", computador.Marca)
+console.log("Procesador: ", computador.Procesador)
+console.log("Ram: ", computador.Ram)
+console.log("Almacenamiento: ", computador.Almacenamiento)
